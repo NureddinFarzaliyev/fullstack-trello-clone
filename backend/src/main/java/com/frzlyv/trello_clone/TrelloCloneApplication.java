0e837c437f21520eaa47977a512728db30bb981b
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-07-31 09:16:11 +0400

    init: dependencies

---
+++ backend/src/main/java/com/frzlyv/trello_clone/TrelloCloneApplication.java
@@ -0,0 +1,13 @@
+package com.frzlyv.trello_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrelloCloneApplication.class, args);
+  }
+
+}
