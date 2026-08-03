797da08bfcb6fd31b1642ea4aa9de42e48e216e9
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-08-03 15:31:47 +0400

    feat:backend: openapi

--- backend/src/main/java/com/frzlyv/trello_clone/config/SecurityConfig.java
+++ backend/src/main/java/com/frzlyv/trello_clone/config/SecurityConfig.java
@@ -32,7 +32,9 @@ public class SecurityConfig {
     http
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated())
+            .requestMatchers("/api/v1/auth/**").permitAll()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+            .anyRequest().authenticated())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
