a5ffad97cfd0d44ffe5edaeea56c898e4a7a2543
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-08-14 09:35:01 +0400

    feat:backend: card entity

---
+++ backend/src/main/java/com/frzlyv/trello_clone/features/card/domain/CardEntity.java
@@ -0,0 +1,49 @@
+package com.frzlyv.trello_clone.features.card.domain;
+
+import java.util.Date;
+
+import com.frzlyv.trello_clone.features.column.domain.ColumnEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * CardEntity
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "cards")
+public class CardEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "card_id_seq")
+  Long id;
+
+  @Column(nullable = false)
+  String title;
+
+  String description;
+
+  Long position;
+
+  Date due;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "column_id", nullable = false)
+  ColumnEntity column;
+
+}
